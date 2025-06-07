57cd903344cd22a4c4b171effd7b4759d1b12146
NAGIOuni <zaft.freedom.x20a@gmail.com>
2025-06-07 14:38:51 +0900

    first build

---
+++ src/app/list/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <>
+      <h1>これまでの制作物</h1>
+      <div className="">
+        TangoCard
+      </div>
+    </>
+  )
+}
+
+export default List
---
+++ src/app/list/tangoCard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TangoCard = () => {
+  return (
+    <div>TangoCard</div>
+  )
+}
+
+export default TangoCard
